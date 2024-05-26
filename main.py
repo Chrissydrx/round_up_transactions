b18c88b39d66d376bdeeb3d51f89bb39583be943
Christian Matthaei <chrissy.drx@gmail.com>
2024-05-26 17:59:16 +0200

    feature: add custom file_paths

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ value_column_name = "Betrag"
 only_negative_amounts = True
 regex_date = r"\d{2}\.\d{4}"
 csv_file = "data/values.csv"
+ask_for_file_path = False
 
 
 def main():
+    if ask_for_file_path:
+        csv_file = input("Enter the path to the csv file: ")
+
     df = pd.read_csv(csv_file, sep=separator)
 
     # Get month to filter for
-    # date = input("Enter a month with a year: ")
-    date = "04.2024"
+    date = input("Enter a month with a year: ")
     if not re.match(regex_date, date):
         print("Invalid date format.")
         return
@@ -23,7 +26,7 @@ def main():
     df = df[df[date_column_name].str.contains(date)]
 
     # Iterate over all rows and sum the difference for each row when rounding the amount up to the nearest euro / dollar.
-    sum = 0
+    final_sum = 0
     for index, row in df.iterrows():
         amount = float(row[value_column_name].replace(",", "."))
         amount *= 100
@@ -38,11 +41,11 @@ def main():
         if amount == 0:
             continue
 
-        sum += 100 - amount
+        final_sum += 100 - amount
 
-    sum /= 100
+    final_sum /= 100
 
-    print("Sum: " + str(sum))
+    print("Sum: " + str(final_sum))
 
 
 if __name__ == "__main__":
