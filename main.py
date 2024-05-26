6ac88d4a480e5f0a083e2b12ef817507adf5ce40
Christian Matthaei <chrissy.drx@gmail.com>
2024-05-26 17:33:18 +0200

    feature: basic caluclation

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
+import pandas as pd
+import re
+
+separator = ";"
+date_column_name = "Buchungstag"
+value_column_name = "Betrag"
+only_negative_amounts = True
+regex_date = r"\d{2}\.\d{4}"
+csv_file = "data/values.csv"
+
+
 def main():
-    print("Test!")
+    df = pd.read_csv(csv_file, sep=separator)
+
+    # Get month to filter for
+    date = input("Enter a month with a year: ")
+    if not re.match(regex_date, date):
+        print("Invalid date format.")
+        return
+
+    # Filter df for dates that include 05.2024
+    df = df[df[date_column_name].str.contains(date)]
+
+    # Iterate over all rows and sum the difference for each row when rounding the amount up to the nearest euro / dollar.
+    sum = 0
+    for index, row in df.iterrows():
+
+        amount = float(row[value_column_name].replace(",", "."))
+
+        if only_negative_amounts and amount > 0:
+            continue
+
+        amount = amount - int(amount)
+        amount = round(amount, 2)
+
+        amount = abs(amount)
+
+        if amount == 0:
+            continue
+
+        sum += 1 - amount
+
+        sum = round(sum, 2)
+
+    print("Sum: " + str(sum))
 
 
 if __name__ == "__main__":
